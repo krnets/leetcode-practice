d7bced3dca9d351109b0efffcfa25a39bf892316
krnets <cmantheo@gmail.com>
2019-12-12 19:03:16 +1100

    renamed 2 files, added new solution to IntToRoman

--- 12. Integer to Roman.js
+++ 12. Integer to Roman.js
@@ -118,6 +118,27 @@ var intToRoman = function (num) {
 // };
 // sample 108 ms submission
 
+// function intToRoman(num) {
+//     const map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+//     let result = '';
+//     Object.entries(map).forEach(([letter, n]) => {
+//         result += letter.repeat(Math.floor(num / n));
+//         num %= n;
+//     });
+//     return result;
+// }
+
+
+// function intToRoman(num) {
+//     const map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+//     return Object.entries(map).reduce((result, [letter, n]) => {
+//         result += letter.repeat(Math.floor(num / n));
+//         num %= n;
+//         return result;
+//     }, '');
+// }
+
+
 q = intToRoman(1) // 'I'
 q
 q = intToRoman(4) // 'IV'
